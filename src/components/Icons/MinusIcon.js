68ec2a517ea046cbd68b5b6814ed98155a44042e
Cristian Bernal <cristianxsa15@gmail.com>
2021-01-28 00:09:43 +0100

    Updated Icons

--- src/components/Icons/MinusIcon.js
+++ src/components/Icons/MinusIcon.js
@@ -3,13 +3,19 @@ import React from "react";
 const MinusIcon = () => {
   return (
     <svg
-      width="14"
-      height="2"
-      viewBox="0 0 14 2"
+      width="16"
+      height="4"
+      viewBox="0 0 16 4"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M0 1H14" stroke="black" strokeWidth="2" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15 2.5C15 2.77614 14.7761 3 14.5 3H1.5C1.22386 3 1 2.77614 1 2.5V1.5C1 1.22386 1.22386 1 1.5 1H14.5C14.7761 1 15 1.22386 15 1.5V2.5Z"
+        fill="black"
+        stroke="black"
+      />
     </svg>
   );
 };
